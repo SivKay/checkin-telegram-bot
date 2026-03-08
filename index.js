05938e43b29e7e7937572400fec0eeacdddd6441
SivKay <11jsivkay@gmail.com>
2026-03-08 18:16:45 +0700

    update pg config

--- index.js
+++ index.js
@@ -10,7 +10,8 @@ const pool = new Pool({
   host: process.env.DB_HOST,
   database: process.env.DB_NAME,
   password: process.env.DB_PASSWORD,
-  port: process.env.PORT,
+  port: Number(process.env.PORT),
+  ssl: { rejectUnauthorized: false },
 });
 
 pool
